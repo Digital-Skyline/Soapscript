ab4066ce53690745b052722a48465f37be5ebde4
Thomas <thomasw2326@gmail.com>
2017-11-26 13:21:44 -0800

    Added Jasmin jar and main file

---
+++ src/Soapscript.java
@@ -0,0 +1,30 @@
+/***
+ * Excerpted from "The Definitive ANTLR 4 Reference",
+***/
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class Soapscript
+{
+    public static void main(String[] args) throws Exception
+    {
+        String inputFile = null;
+
+        if (args.length > 0) inputFile = args[0];
+        InputStream is = (inputFile != null)
+                                ? new FileInputStream(inputFile)
+                                : System.in;
+
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        PclLexer lexer = new PclLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        PclParser parser = new PclParser(tokens);
+        ParseTree tree = parser.program();
+
+        CompilerVisitor compiler = new CompilerVisitor();
+        compiler.visit(tree);
+    }
+}
